bb2975897ea790d2e1410cb2b19203dea448debc
SasukeXDev <srk100m@gmail.com>
2025-08-03 23:18:20 +0530

    Update cmds.py
    af

--- TG/cmds.py
+++ TG/cmds.py
@@ -9,6 +9,7 @@ import pyrogram.errors
 
 from bot import Bot, Vars, logger
 
+
 import random
 from Tools.db import *
 from Tools.my_token import *
@@ -81,7 +82,7 @@ async def start(client, message):
                                          InlineKeyboardButton(" Close ", callback_data = "close")
                                      ]]))
 
-@client.on_message(filters.command("start") & filters.private)
+@Bot.on_message(filters.command("start") & filters.private)
 async def start_handler(client, message):
     effects = [
         "5046509860389126442",  # 🎉
