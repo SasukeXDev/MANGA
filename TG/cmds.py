8884a0ca01099d3f427d514cccedcaa5d7f525c3
SasukeXDev <srk100m@gmail.com>
2025-08-06 00:08:27 +0530

    Update cmds.py
    dh

--- TG/cmds.py
+++ TG/cmds.py
@@ -1,4 +1,4 @@
-from pyrogram import filters, Client
+from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
