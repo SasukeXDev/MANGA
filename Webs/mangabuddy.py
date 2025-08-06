b6d945f969c744463e75eeee6fb0c775f55c755d
SasukeXDev <srk100m@gmail.com>
2025-08-07 00:46:00 +0530

    Update mangabuddy.py
    egr

--- Webs/mangabuddy.py
+++ Webs/mangabuddy.py
@@ -1,145 +1,176 @@
-import re
-from dataclasses import dataclass
-from typing import List, AsyncIterable
-from urllib.parse import urlparse, urljoin, quote, quote_plus
 
+from .scraper import Scraper
+import json 
 from bs4 import BeautifulSoup
 
-
-from Webs.client import MangaClient, MangaCard, MangaChapter
-
-
-@dataclass
-class MangaBuddyCard(MangaCard):
-    read_url: str
-
-    def get_url(self):
-        return self.read_url
+from loguru import logger
 
 
-class MangaBuddyClient(MangaClient):
-    base_url = urlparse("https://mangabuddy.com/")
-    search_url = urljoin(base_url.geturl(), "search")
-    search_param = 'q'
-    home_page = urljoin(base_url.geturl(), "home-page")
-    img_server = "https://s1.mbbcdnv1.xyz/file/img-mbuddy/manga/"
-
-    pre_headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0'
+class MangaBuddyWebs(Scraper):
+  def __init__(self):
+    super().__init__()
+    self.url = "https://comick.app"
+    self.bg = None
+    self.sf = "ck"
+    self.headers = {
+      "Accept": "application/json",
+      "Referer": "https://comick.cc",
+      "User-Agent": "Tachiyomi Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36",
     }
+    self.search_query = dict()
+    
+  async def get_information(self, slug, data):
+    url = f"https://api.comick.fun/comic/{slug}/?t=0"
+    series = await self.get(url, cs=True, rjson=True, headers=self.headers)
+    
+    title = series["comic"]["title"]
+    status = series["comic"]["status"]
+    status = {1: "Ongoing", 2: "Completed", 3: "Cancelled", 4: "On Hiatus"}.get(
+            status, "N/A"
+    )
+    rating = series["comic"]["bayesian_rating"]
+    file_key = series["comic"]["md_covers"][0]["b2key"]
+    cover = f"https://meo.comick.pictures/{file_key}"
+    url = f"https://comick.io/comic/{slug}"
+    data['url'] = url
+    
+    # genres_list = [i['name'] for i in series['genres']] # api changes broke this
+    genres_list = [
+        i["md_genres"]["name"] for i in series["comic"]["md_comic_md_genres"]
+    ]
+    genres = ", ".join(genres_list) or "N/A"
+    # nsfw = series['comic']['hentai'] or "None"
+    try: desc = series["comic"]["desc"]
+    except: desc = "N/A"
+    desc = desc if desc else "N/A"
 
-    def __init__(self, *args, name="MangaBuddy", **kwargs):
-        super().__init__(*args, name=name, headers=self.pre_headers, **kwargs)
+    last_chap = series["comic"]["last_chapter"] or "N/A"
+    content_rating = series["comic"]["content_rating"].capitalize() or "N/A"
+    #demographic = series["comic"]["demographic"] or "N/A"
+    #demographic = {1: "Shonen", 2: "Shojo", 3: "Seinen", 4: "Josei"}.get(
+        #demographic, "N/A"
+    #)
+    year = series["comic"]["year"] or "N/A"
+    authors_list = [a["name"] for a in series["authors"]]
+    authors = ", ".join(authors_list) or "N/A"
+    artist_list = [a["name"] for a in series["artists"]]
+    artists = ", ".join(artist_list) or "N/A"
 
-    def mangas_from_page(self, page: bytes):
-        bs = BeautifulSoup(page, "html.parser")
+    msg = f"<b>{title} (<code>{year}</code>)</b>\n\n" #msg += f"<b>Alt Names:</b> <code>{alts}</code>\n"
+    #msg += f"<b>NSFW:</b> <code>{nsfw}</code>\n"
+    msg += f"<b>Rating:</b> <code>{rating}</code>\n" #msg += f"<b>Content Type:</b> <code>{content_rating}</code>\n" #msg += f"<b>Demographic:</b> <code>{demographic}</code>\n"
+    msg += f"<b>Genres:</b> <code>{genres}</code>\n"
+    msg += f"<b>Last Chapter:</b> <code>{last_chap}</code>\n"
+    msg += f"<b>Status:</b> <code>{status}</code>\n"
+    msg += f"<b>Authors:</b> <code>{authors}</code>\n"
+    if authors != artists:
+        msg += f"<b>Artists:</b> <code>{artists}</code>\n"
 
-        cards = bs.find_all("div", {"class": "book-item"})
+    su = int(len(desc)) + int(len(msg))
+    if su < 1024:
+        msg += f"\n<i>{desc}</i>"
+        if len(msg) > 1024:
+            msg = msg[:1023]
+    else:
+        msg += f"\n<b><a href='{url}'>..</a></b></i>"
 
-        mangas = [card.a for card in cards if card.a is not None]
-        names = [manga.get("title").strip() for manga in mangas]
-        read_url = [urljoin(self.base_url.geturl(), manga.get('href').strip()) for manga in mangas]
-        url = [f'https://mangabuddy.com/api/manga{manga.get("href").strip()}/chapters?source=detail' for manga in mangas]
-        images = [manga.find("img").get('data-src').strip() for manga in mangas]
-
-        mangas = [MangaBuddyCard(self, *tup) for tup in zip(names, url, images, read_url)]
-
+    if len(msg) > 1024:
+        msg = msg[:1023]
+    
+    data['msg'] = msg
+    data['poster'] = cover
+      
+  async def search(self, query: str = ""):
+    if query.lower in self.search_query:
+        mangas = self.search_query[query.lower]
         return mangas
+    
+    url = f"https://api.comick.fun/v1.0/search/?type=comic&page=1&limit=8&q={query}&t=false"
+    mangas = await self.get(url, cs=True, rjson=True, headers=self.headers)
+    for manga in mangas:
+      url = f"https://comick.io/comic/{manga['slug']}"
 
-    def chapters_from_page(self, page: bytes, manga: MangaCard = None):
-        bs = BeautifulSoup(page, "html.parser")
+      file_key = manga["md_covers"][0]["b2key"]
+      
+      images = f"https://meo.comick.pictures/{file_key}"
+      
+      manga['url'] = url
+      manga['poster'] = images
+    
+    self.search_query[query.lower] = mangas
+    
+    return mangas
+    
+  async def get_chapters(self, data, page: int=1):
+    results = {}
+    url = f"https://api.comick.fun/comic/{data['hid']}/chapters?lang=en&page={str(page)}"
+    
+    results = await self.get(url, cs=True, rjson=True, headers=self.headers)
+    if results:
+      await self.get_information(data['slug'], results)
+      
+      results['title'] = data['title']
+    
+    return results
+  
+  def iter_chapters(self, data):
+    if not data or not 'chapters' in data:
+      return []
+    
+    chapters_list = []
+    for chapter in data['chapters']:
+      title = chapter.get("title", None)
+      #title = title.replace("None", "")
+      title = f"{chapter['chap']} - {title}" if title else f"Chapter {chapter['chap']}"
+      try: md_group = chapter.get("group_name", ["None"])[0]
+      except: md_group = None
+      title = f"{title} ({md_group})" if md_group else title
+      chapters_list.append({
+        "title": title,
+        "url": f"{data['url']}/{chapter['hid']}-chapter-{chapter['chap']}-en",
+        "slug": chapter['hid'],
+        "manga_title": data['title'],
+        "group_name": md_group,
+        "poster": data['poster'] if 'poster' in data else None,
+      })
+    
+    return chapters_list
+    
+  async def get_pictures(self, url, data=None):
+    response = await self.get(url, cs=True, headers=self.headers)
+    bs = BeautifulSoup(response, "html.parser")
+    container = bs.find("script", {"id": "__NEXT_DATA__"})
 
-        ul = bs.find('ul', {'id': 'chapter-list'})
+    con = container.text.strip()
+    con = json.loads(con)
 
-        lis = ul.findAll('li')
-        a_elems = [li.find('a') for li in lis]
+    images = con["props"]["pageProps"]["chapter"]["md_images"]
+    images_url = [f"https://meo.comick.pictures/{image['b2key']}" for image in images]
 
-        links = [urljoin(self.base_url.geturl(), a.get('href')) for a in a_elems]
-        texts = [a.findNext('strong', {'class': 'chapter-title'}).text.strip() for a in a_elems]
-
-        return list(map(lambda x: MangaChapter(self, x[0], x[1], manga, []), zip(texts, links)))
-
-    def updates_from_page(self, page: bytes):
-        bs = BeautifulSoup(page, "html.parser")
-
-        div = bs.find('div', {'class': 'container__left'})
-
-        manga_items = div.findAll('div', {'class': 'book-item'})
-
-        urls = dict()
-
-        for manga_item in manga_items:
-
-            manga_url_part = manga_item.findNext('a').get('href')
-            manga_url = f'https://mangabuddy.com/api/manga{manga_url_part}/chapters?source=detail'
-
-            chapter_item = manga_item.findNext("div", {"class": "chap-item"})
-            if not chapter_item or not chapter_item.a:
-                continue
-            chapter_url = urljoin(self.base_url.geturl(), chapter_item.a.get('href'))
-
-            if manga_url not in urls:
-                urls[manga_url] = chapter_url
-
-        return urls
-
-    async def pictures_from_chapters(self, content: bytes, response=None):
-
-        regex = rb"var chapImages = '(.*)'"
-
-        imgs = re.findall(regex, content)[0].decode().split(',')
-
-        images_url = [img for img in imgs]
-
-        return images_url
-
-    async def search(self, query: str = "", page: int = 1) -> List[MangaCard]:
-        request_url = self.search_url
-
-        if query:
-            request_url = f'{request_url}?{self.search_param}={quote_plus(query)}'
-
-        content = await self.get_url(request_url)
-
-        return self.mangas_from_page(content)[(page - 1) * 20:page * 20]
-
-    async def get_chapters(self, manga_card: MangaCard, page: int = 1) -> List[MangaChapter]:
-
-        request_url = f'{manga_card.url}'
-
-        content = await self.get_url(request_url)
-
-        return self.chapters_from_page(content, manga_card)[(page - 1) * 20:page * 20]
-
-    async def iter_chapters(self, manga_url: str, manga_name) -> AsyncIterable[MangaChapter]:
-        manga_card = MangaCard(self, manga_name, manga_url, '')
-
-        request_url = f'{manga_card.url}'
-
-        content = await self.get_url(request_url)
-
-        for chapter in self.chapters_from_page(content, manga_card):
-            yield chapter
-
-    async def contains_url(self, url: str):
-        return url.startswith(self.base_url.geturl())
-
-    async def check_updated_urls(self, last_chapters: List[MangaChapter]):
-
-        content = await self.get_url(self.home_page)
-
-        updates = self.updates_from_page(content)
-
-        updated = [lc.url for lc in last_chapters if updates.get(lc.url) and updates.get(lc.url) != lc.chapter_url]
-        not_updated = [lc.url for lc in last_chapters if not updates.get(lc.url)
-                       or updates.get(lc.url) == lc.chapter_url]
-
-        return updated, not_updated
-
-    async def get_cover(self, manga_card: MangaCard, *args, **kwargs):
-        headers = {**self.pre_headers, 'Referer': self.base_url.geturl()}
-        return await super(MangaBuddyClient, self).get_cover(manga_card, *args, headers=headers, **kwargs)
-
-    async def get_picture(self, manga_chapter: MangaChapter, url, *args, **kwargs):
-        headers = {**self.pre_headers, 'Referer': self.base_url.geturl()}
-        return await super(MangaBuddyClient, self).get_picture(manga_chapter, url, *args, headers=headers, **kwargs)
+    return images_url
+    
+  async def get_updates(self, page:int = 1):
+    output = []
+    while page <= 5:
+      url = f"https://api.comick.fun/chapter?page={page}&device-memory=8&order=new"
+      results = await self.get(url, cs=True, rjson=True, headers=self.headers)
+      
+      for data in results:
+        url = f"https://comick.io/comic/{data['md_comics']['slug']}"
+        file_key = data["md_comics"]['md_covers'][0]["b2key"]
+        
+        images = f"https://meo.comick.pictures/{file_key}"
+        chapter_url = f"{url}/{data['hid']}-chapter-{data['chap']}-en"
+        
+        data['poster'] = images
+        data['chapter_url'] = chapter_url
+        
+        data['url'] = url
+        data['manga_title'] = data['md_comics']['title']
+        data['title'] = f"Chapter {data['chap']}"
+        
+        output.append(data)
+      
+      page += 1
+      
+    return output
